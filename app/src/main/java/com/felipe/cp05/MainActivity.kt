105408e93adcf3cda7e2d0ff1e17f71d3bee8e98
Felipe Palmeira <pro.felipefeitosa.jobs@gmail.com>
2024-10-04 20:25:21 -0300

    Configurando a MainActivity.kt para executar com o layout pronto

--- app/src/main/java/com/felipe/cp05/MainActivity.kt
+++ app/src/main/java/com/felipe/cp05/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.felipe.cp05
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.felipe.cp05.ui.theme.Cp05Theme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
 
